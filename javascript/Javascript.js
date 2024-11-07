6789de147e8bb57127b50389e190190b0023ec90
Breslin Reyes <breslinreyesfamilia@gmail.com>
2024-11-07 15:17:57 -0400

    Add files via upload

---
+++ javascript/Javascript.js
@@ -0,0 +1,33 @@
+const openModal = document.querySelector('.hero__cta');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal__close');
+
+openModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.add('modal--show');
+});
+
+closeModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal--show');
+});
+
+
+function saludar(){
+
+    window.alert('Felicidades ya estas registrado');
+}
+
+
+/// este code es para el menu hamburguesa
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
